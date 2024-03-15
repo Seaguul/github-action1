9768e090cdc2d0e3813c0507460d22099e18f269
Kevin Oswald <103310432+Seaguul@users.noreply.github.com>
2024-03-15 11:52:38 +0700

    Update model.go

--- latihan1/models/model.go
+++ latihan1/models/model.go
@@ -1,2 +1,9 @@
 package models
 
+type User struct {
+	ID       int    `json: "id"`
+	Name     string `json: "name"`
+	Age      int    `json: "Age"`
+	Address  string `json: "Address"`
+	UserType int    `json: "UserType"`
+}
