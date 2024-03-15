ebb33430225b300668e6b61837cb46312a82a9ae
Kevin Oswald <103310432+Seaguul@users.noreply.github.com>
2024-03-15 11:10:18 +0700

    Add files via upload

---
+++ latihan1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"latihan1/controllers"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 8888")
+	log.Println("Connected to port 8888")
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
