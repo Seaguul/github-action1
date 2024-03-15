379d5b07ac3c6ca14529c0f7ab2aa0e84fc923c5
Kevin Oswald <103310432+Seaguul@users.noreply.github.com>
2024-03-15 11:49:41 +0700

    Update model.go

--- latihan1/models/model.go
+++ latihan1/models/model.go
@@ -2,14 +2,3 @@ package models
 
 
 
-type UserResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Data    User   `json: "data"`
-}
-
-type UsersResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Data    []User `json: "data"`
-}
