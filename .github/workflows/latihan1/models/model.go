9e4078e15b7efd9847a8bfaaeb2430011e1986f5
Kevin Oswald <103310432+Seaguul@users.noreply.github.com>
2024-03-15 11:05:32 +0700

    Add files via upload

---
+++ .github/workflows/latihan1/models/model.go
@@ -0,0 +1,21 @@
+package models
+
+type User struct {
+	ID       int    `json: "id"`
+	Name     string `json: "name"`
+	Age      int    `json: "Age"`
+	Address  string `json: "Address"`
+	UserType int    `json: "UserType"`
+}
+
+type UserResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    User   `json: "data"`
+}
+
+type UsersResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []User `json: "data"`
+}
